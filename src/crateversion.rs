89366198391b92f1049dfe1cf37c72b58f688c6c
David Tolnay <dtolnay@gmail.com>
2023-02-01 17:38:48 -0800

    Implement Ord for CrateVersion by comparing version

--- src/crateversion.rs
+++ src/crateversion.rs
@@ -1,4 +1,5 @@
 use semver::Version;
+use std::cmp::Ordering;
 
 pub struct CrateVersion {
     pub version: Version,
@@ -7,3 +8,39 @@ pub struct CrateVersion {
 }
 
 pub type Checksum = [u8; 32];
+
+impl Ord for CrateVersion {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let CrateVersion {
+            version: this_version,
+            checksum: _,
+        } = self;
+        let CrateVersion {
+            version: other_version,
+            checksum: _,
+        } = other;
+        this_version.cmp(other_version)
+    }
+}
+
+impl PartialOrd for CrateVersion {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(Ord::cmp(self, other))
+    }
+}
+
+impl Eq for CrateVersion {}
+
+impl PartialEq for CrateVersion {
+    fn eq(&self, other: &Self) -> bool {
+        let CrateVersion {
+            version: this_version,
+            checksum: _,
+        } = self;
+        let CrateVersion {
+            version: other_version,
+            checksum: _,
+        } = other;
+        this_version == other_version
+    }
+}
